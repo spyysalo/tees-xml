2e3108dc97b5ba1370b10f72dc610ddc1782cda4
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-01 16:31:44 +0200

    Initial import

---
+++ scripts/converttees.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import gzip
+import xml.etree.ElementTree as ET
+
+from logging import warn, error
+
+from teesxml import Document, Sentence, Entity, Token, Dependency
+
+
+DEFAULT_OUTPUT='converted'
+
+
+def argparser():
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument('files', metavar='FILE', nargs='+',
+                    help='Input TEES XML files')
+    ap.add_argument('-l', '--limit', default=100, type=int,
+                    help='Maximum number of documents to process')
+    ap.add_argument('-o', '--output-dir', default=DEFAULT_OUTPUT,
+                    help='Output directory (default {})'.format(DEFAULT_OUTPUT))
+    ap.add_argument('-s', '--sentences', default=False, action='store_true',
+                    help='Output one sentence per file')
+    ap.add_argument('-p', '--pairs-only', default=False, action='store_true',
+                    help='Only output sentences with entity pairs')
+    ap.add_argument('-d', '--no-deps', default=False, action='store_true',
+                    help='Do not output dependencies')
+    ap.add_argument('-t', '--no-tokens', default=False, action='store_true',
+                    help='Do not output tokens (implies --no-deps)')
+    return ap
+
+
+def process_sentence(sentence, fn, options):
+    s = Sentence.from_xml(sentence)
+    if options.pairs_only and len(s.entities) < 2:
+        return 0
+    txt_fn = os.path.join(options.output_dir, s.id + '.txt')
+    ann_fn = os.path.join(options.output_dir, s.id + '.ann')
+    with open(txt_fn, 'w') as f:
+        print(s.text, file=f)
+    with open(ann_fn, 'w') as f:
+        for e in s.entities:
+            print(e.to_ann(), file=f)
+        if not options.no_tokens:
+            for t in s.tokens:
+                print(t.to_ann(), file=f)
+            if not options.no_deps:
+                for d in s.dependencies:
+                    print(d.to_ann(), file=f)
+    return 1
+
+
+def write_annotations(sentence, out, base_offset, options):
+    for e in sentence.entities:
+        print(e.to_ann(base_offset), file=out)
+    if not options.no_tokens:
+        for t in sentence.tokens:
+            print(t.to_ann(base_offset), file=out)
+        if not options.no_deps:
+            for d in sentence.dependencies:
+                print(d.to_ann(), file=out)
+
+
+def write_sentence(sentence, fn, options):
+    txt_fn = os.path.join(options.output_dir, sentence.id + '.txt')
+    ann_fn = os.path.join(options.output_dir, sentence.id + '.ann')
+    with open(txt_fn, 'w') as out:
+        print(sentence.text, file=out)
+    with open(ann_fn, 'w') as out:
+        write_annotations(sentence, out, 0, options)
+
+
+def write_document(document, fn, options):
+    if options.sentences:
+        for s in document.sentences:
+            write_sentence(s, fn, options)
+    else:
+        txt_fn = os.path.join(options.output_dir, document.id + '.txt')
+        ann_fn = os.path.join(options.output_dir, document.id + '.ann')
+        with open(txt_fn, 'w') as out:
+            print(document.text, file=out)
+        with open(ann_fn, 'w') as out:
+            for s in document.sentences:
+                write_annotations(s, out, s.start, options)
+
+
+def read_document(element, fn, options):
+    return Document.from_xml(element)
+
+
+def process_stream(stream, fn, options):
+    count = 0
+    for event, element in stream:
+        if count >= options.limit:
+            break
+        if event == 'end' and element.tag == 'document':
+            document = read_document(element, fn, options)
+            write_document(document, fn, options)
+            element.clear()
+            count += 1
+        else:
+            pass    # TODO others?
+
+
+def process(fn, options):
+    if not fn.endswith('.gz'):
+        process_stream(ET.iterparse(fn), fn, options)
+    else:
+        with gzip.GzipFile(fn) as stream:
+            process_stream(ET.iterparse(stream), fn, options)
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    for fn in args.files:
+        process(fn, args)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ scripts/teesxml.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import xml.etree.ElementTree as ET
+
+from itertools import chain
+from collections import defaultdict
+from logging import info, warn, error
+
+
+class FormatError(Exception):
+    pass
+
+
+def raise_amended(exception, message):
+    # https://stackoverflow.com/a/6062799
+    raise type(exception), type(exception)(exception.message + ' ' + message),\
+        sys.exc_info()[2]
+
+
+class Document(object):
+    def __init__(self, id_, orig_id, text, sentences):
+        self.id = id_
+        self.orig_id = orig_id
+        self.text = text
+        self.sentences = sentences
+        for s in self.sentences:
+            s.document = self
+        self.assign_uids()
+
+    def assign_uids(self, next_free_idx=None):
+        """Generate document-level unique IDs replacing non-unique TEES IDs."""
+        if next_free_idx is None:
+            next_free_idx = defaultdict(lambda: 1)            
+        for s in self.sentences:
+            s.assign_uids(next_free_idx)
+        
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        orig_id = element.attrib['origId']
+        text = element.attrib['text']
+        sentences = []
+        for sentence in element.findall('sentence'):
+            try:
+                sentences.append(Sentence.from_xml(sentence))
+            except Exception, e:
+                raise_amended(e, 'in document {}'.format(id_))
+        return cls(id_, orig_id, text, sentences)
+
+
+class Sentence(object):
+    def __init__(self, id_, text, offset, entities, tokens, phrases,
+                 dependencies):
+        self.id = id_
+        self.text = text
+        self.start, self.end = map(int, offset.split('-'))
+        assert self.start <= self.end
+        self.offset = offset
+        self.entities = entities
+        self.tokens = tokens
+        self.phrases = phrases
+        self.dependencies = dependencies
+        for i in chain(self.tokens, self.phrases, self.entities,
+                       self.dependencies):
+            i.sentence = self
+        self.token_by_id = { t.id: t for t in self.tokens }
+        self.document = None
+
+    def assign_uids(self, next_free_idx):
+        for i in chain(self.tokens, self.phrases, self.entities,
+                       self.dependencies):
+            i.assign_uid(next_free_idx)
+
+    def get_token_uid(self, id_):
+        return self.token_by_id.get(id_).uid
+
+    def find_head(self, start, end):
+        """Return Token most likely to be head of given span."""
+        spanned = [
+            t for t in self.tokens
+            if t.start < end and t.end > start
+        ]
+        max_score = max(t.head_score for t in spanned)
+        max_scoring = [t for t in spanned if t.head_score == max_score]
+        if len(max_scoring) > 1:
+            info('multiple tokens with max head_score, '+
+                 'arbitrarily choosing last: {}'.format(max_scoring))
+        return max_scoring[-1]
+            
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        text = element.attrib['text']
+        offset = element.attrib['charOffset']
+        entities, tokens, phrases, dependencies = [], [], [], []
+        for entity in element.findall('evex_entity'):
+            try:
+                entities.append(Entity.from_xml(entity))
+            except Exception, e:
+                raise_amended(e, 'in sentence {}'.format(id_))
+        for analyses in element.findall('analyses'):
+            for tokenization in analyses.findall('tokenization'):
+                for token in tokenization.findall('token'):
+                    try:
+                        tokens.append(Token.from_xml(token))
+                    except Exception, e:
+                        raise_amended(e, 'in sentence {}'.format(id_))
+            for parse in analyses.findall('parse'):
+                for dependency in parse.findall('dependency'):
+                    try:
+                        dependencies.append(Dependency.from_xml(dependency))
+                    except Exception, e:
+                        raise_amended(e, 'in sentence {}'.format(id_))
+                for phrase in parse.findall('phrase'):
+                    try:
+                        phrases.append(Phrase.from_xml(phrase))
+                    except Exception, e:
+                        raise_amended(e, 'in sentence {}'.format(id_))
+        return cls(id_, text, offset, entities, tokens, phrases, dependencies)
+
+
+class Span(object):
+    def __init__(self, offset):
+        self.start, self.end = map(int, offset.split('-'))
+        assert self.start <= self.end
+        self.offset = offset
+        self.type = None
+        self.sentence = None
+        self.uid = None
+
+    def assign_uid(self, next_free_idx):
+        self.uid = 'T{}'.format(next_free_idx['T'])
+        next_free_idx['T'] += 1
+
+    def to_ann(self, base_offset=0):
+        start = self.start + base_offset
+        end = self.end + base_offset
+        return '{}\t{} {} {}\t{}'.format(self.uid, self.type, start, end,
+                                         self.text)
+
+
+class Entity(Span):
+    def __init__(self, id_, type_, offset, text, orig_id):
+        super(Entity, self).__init__(offset)
+        self.id = id_
+        self.type = type_
+        self.text = text
+        self.orig_id = orig_id
+
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        type_ = element.attrib['entity_type']
+        offset = element.attrib['charOffset']
+        text = element.attrib['text']
+        orig_id = element.attrib['origId']
+        return cls(id_, type_, offset, text, orig_id)
+
+
+class Token(Span):
+    def __init__(self, id_, pos, offset, text, head_score):
+        super(Token, self).__init__(offset)
+        self.id = id_
+        self.pos = pos
+        self.text = text
+        self.head_score = head_score
+        self.type = 'Token'
+
+    def __str__(self):
+        return '{}/{}'.format(self.text, self.pos)
+
+    def __repr__(self):
+        return 'Token({}, {}, {}, {}, {})'.format(
+            self.id, self.pos, self.offset, self.text, self.head_score)
+
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        pos = element.attrib['POS']
+        offset = element.attrib['charOffset']
+        text = element.attrib['text']
+        head_score = element.get('headScore', -99)    # TODO magic number
+        return cls(id_, pos, offset, text, head_score)
+
+
+class Dependency(object):
+    def __init__(self, id_, type_, start, end):
+        self.id = id_
+        self.type = type_
+        self.start = start
+        self.end = end
+        self.sentence = None
+        self.uid = None
+
+    def assign_uid(self, next_free_idx):
+        self.uid = 'R{}'.format(next_free_idx['R'])
+        next_free_idx['R'] += 1
+
+    def to_ann(self):
+        s_id = self.sentence.get_token_uid(self.start)
+        e_id = self.sentence.get_token_uid(self.end)
+        return '{}\t{} Arg1:{} Arg2:{}'.format(self.uid, self.type, s_id, e_id)
+
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        start = element.attrib['t1']
+        end = element.attrib['t2']
+        type_ = element.attrib['type']
+        return cls(id_, type_, start, end)
+
+
+class Phrase(Span):
+    def __init__(self, id_, type_, offset):
+        super(Phrase, self).__init__(offset)
+        self.id = id_
+        self.type = type_
+
+    @classmethod
+    def from_xml(cls, element):
+        id_ = element.attrib['id']
+        type_ = element.attrib['type']
+        offset = element.attrib['charOffset']
+        return cls(id_, type_, offset)
