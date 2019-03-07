843c925b17e2daaa281f7339da91f27be1973db7
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-07 17:10:01 +0200

    Add option to pick values to show

--- scripts/catsqlite.py
+++ scripts/catsqlite.py
@@ -8,23 +8,30 @@ from sqlitedict import SqliteDict
 def argparser():
     from argparse import ArgumentParser
     ap = ArgumentParser(description='List values in SQLiteDict DB.')
-    ap.add_argument('-n', '--listname', default=False, action='store_true')
-    ap.add_argument('db', nargs='+')
+    ap.add_argument('-k', '--showkeys', default=False, action='store_true',
+                    help='include keys in output')
+    ap.add_argument('db', metavar='DB', help='database file')
+    ap.add_argument('keys', metavar='KEY', nargs='*', help='keys to look up')
     return ap
 
 
 def list_db(dbname, options):
     with SqliteDict(dbname) as db:
-        for k, v in db.iteritems():
-            if options.listname:
-                print('==> {} <=='.format(k))
-            print(v)
+        if not options.keys:
+            for k, v in db.iteritems():
+                if options.showkeys:
+                    print('==> {} <=='.format(k))
+                print(v)
+        else:
+            for k in options.keys:
+                if options.showkeys:
+                    print('==> {} <=='.format(k))
+                print(db[k])
 
 
 def main(argv):
     args = argparser().parse_args(argv[1:])
-    for dbname in args.db:
-        list_db(dbname, args)
+    list_db(args.db, args)
     return 0
 
 
