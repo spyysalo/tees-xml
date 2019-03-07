0c9b9f2d6e6b15b0b82a1fe67e84843e012e5083
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-07 09:05:53 +0200

    Example

---
+++ run_standoff_example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# https://stackoverflow.com/a/246128
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SCRIPTDIR="$BASEDIR/scripts"
+
+INXML="$BASEDIR/examples/medline15n0572-s10.xml"
+OUTDIR="$BASEDIR/standoff-output"
+
+mkdir -p "$OUTDIR"
+
+python3 "$SCRIPTDIR/converttees.py" "$INXML" -o "$OUTDIR"
+
+echo "Done, output in $OUTDIR" >&2
